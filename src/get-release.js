ba2fcc05dfceb43c30241107a382c42950dc5107
Trais McAllister <tbm0115@hotmail.com>
2023-03-15 14:33:29 -0700

    Create

---
+++ src/get-release.js
@@ -0,0 +1,37 @@
+const core = require('@actions/core');
+const { Octokit } = require("@octokit/rest");
+
+const repository = 'mtconnect/mtconnect_sysml_model';
+const token = core.getInput('token');
+var owner = 'mtconnect';
+var repo = 'mtconnect_sysml_model';
+
+const octokit = (() => {
+  if (token) {
+    return new Octokit({ auth: token,});
+  } else {
+    return new Octokit();
+  }
+})();
+
+async function getRelease() {
+  if (repository){
+    [owner, repo] = repository.split("/");
+  }
+  var releases  = await octokit.repos.listReleases({
+    owner: owner,
+    repo: repo,
+    });
+  releases = releases.data;
+  if (releases.length) {
+    return {
+      'release': releases[0].tag_name,
+      'id': String(releases[0].id),
+      'description': String(releases[0].body)
+    };
+  } else {
+    throw "No valid releases";
+  }
+}
+
+model.export = getRelease;
